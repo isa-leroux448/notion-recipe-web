a2105bdcc71f3adc1a14c34dfa672d1527ced38b
Isabella <isa448@students.cs.ubc.ca>
2023-07-25 00:06:23 -0700

    sorta working

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { Client } from "@notionhq/client"
-import { createChildren } from "./notion";
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const notion = new Client({ auth: process.env.NOTION_SECRET })
-// const databaseId = process.env.NOTION_DATABASE
 
 function App() {
-  async function addPage(title, image, ingredients, steps) {
-    const children = createChildren(ingredients, steps);
-    try {
-        const response = await notion.pages.create({
-            parent: { database_id: databaseId },
-            properties: {
-                title: {
-                    title: [
-                        {
-                            "text": {
-                                "content": title
-                            }
-                        }
-                    ]
-                },
-                Link: {
-                    type: 'url',
-                    url: 'google.com'
-                },
-            },
-            cover: {
-                "type": "external",
-                "external": {
-                    "url": image
-                }
-            },
-            children: children,
-        })
-        console.log(response)
-        console.log("Success! Entry added.")
-    } catch (error) {
-        console.error(error.body)
-    }
-}
-
-
+  
   useEffect(() => {
       // Using fetch to fetch the api from flask server it will be redirected to proxy
       fetch("/data").then((res) =>
           res.json().then((data) => {
             console.log(data);
-            addPage(data.Title, data.Image, data.Ingredients, data.Steps);
           })
       );
   }, []);
